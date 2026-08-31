cd6f3b15c0b06e78e4a4e6ef3ba0ecafd0f35986
francdenis2026-source <francdenis2026@gmail.com>
2026-08-31 12:12:45 -0500

    style(home): load taste layout overrides

--- src/pages/HomeNew2026.tsx
+++ src/pages/HomeNew2026.tsx
@@ -16,6 +16,7 @@ import "./HomeProfessional2026.css";
 import "./HomeFooterCompact2026.css";
 import "./HomeDensityPolish2026.css";
 import "./HomeThemeContrast2026.css";
+import "./HomeTaste2026.css";
 
 const initialCatalog = buildCatalog();
 
